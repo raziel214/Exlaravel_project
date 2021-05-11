3dd17304277d3bef548437155d838969960f2fc5
raziel214 <raziel214@users.noreply.github.com>
2021-05-11 10:35:18 -0500

    first commit

---
+++ resources/views/holamundo.blade.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Hola Mundo </title>
+	</head>
+	<body>
+		<h1>Hola Mundo </h1>
+
+		Este hola mundo se realiza desde laravel como vista 
+	</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+//use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/', 'UserController@index');
+Route::post('users', 'UserController@store')->name('users.store');
+Route::delete('users/{user}', 'UserController@destroy')->name('users.destroy');
+// Route::get('/', function () {
+//     return view('holamundo');
+// });
